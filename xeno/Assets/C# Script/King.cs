39643b1e45e7b5b86bf630b3c6908e2f7e5c5984
KPUX <k-puxe@hotmail.fr>
2017-02-11 18:32:36 +0100

    Base PUX
    Projet echec avant creation du groupe

---
+++ xeno/Assets/C# Script/King.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class King : Piece {
+
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ xeno/Assets/C# Script/Tile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+    public Tile diagonal1;
+    public int number;
+    public Piece piece;
+    public Vector2 coordinates;
+    public Color color;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
